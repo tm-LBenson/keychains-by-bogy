e68534de3ea6f17666dbf22420a7dd332f0aaed8
Lewis Benson <tm.lewisbenson@gmail.com>
2024-11-02 11:53:02 -0400

    adjust

--- src/Products.tsx
+++ src/Products.tsx
@@ -28,7 +28,7 @@ const Products: React.FC = () => {
   }, []);
 
   return (
-    <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+    <div className="mt-6 grid grid-cols-1 lg:grid-cols-2 gap-4">
       {products.map((product) => (
         <div
           key={product.id}
