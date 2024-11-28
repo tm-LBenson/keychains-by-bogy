3f94c419a1b2eecff127ca03101c08ceffcc1832
Lewis Benson <tm.lewis.benson@gmail.com>
2024-11-28 07:58:37 -0500

    remove nav links

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -17,7 +17,6 @@ const Navbar: React.FC = () => {
 
   return (
     <div className="relative">
-      {/* <div className="flex bg-pink-500 shadow-lg  w-full py-4 px-4 sm:px-10  font-[sans-serif] min-h-[80px] tracking-wide  z-50"></div> */}
       <div className="flex bg-pink-500 shadow-lg  w-full top-0 py-4 px-4 sm:px-10  font-[sans-serif] min-h-[70px] tracking-wide  z-50">
         <div className="flex flex-wrap items-center justify-between gap-4 w-full">
           <Link
@@ -59,43 +58,7 @@ const Navbar: React.FC = () => {
                 </svg>
               </button>
             )}
-            <div className="lg:flex lg:items-center lg:w-auto w-full md:w-[50%] mr-5 h-full lg:bg-transparent bg-pink-500 lg:p-0 p-8">
-              <Link
-                to="/#products"
-                className="block lg:inline-block text-black px-3 py-2 rounded text-lg font-medium"
-                onClick={toggleMenu}
-              >
-                Products
-              </Link>
-              {/* <Link
-                to="/about"
-                className="block lg:inline-block text-black px-3 py-2 rounded text-lg font-medium"
-                onClick={toggleMenu}
-              >
-                About Us
-              </Link> */}
-              {/* <Link
-                to="/contact"
-                className="block lg:inline-block text-black px-3 py-2 rounded mr-3 text-lg font-medium"
-                onClick={toggleMenu}
-              >
-                Contact
-              </Link> */}
-            </div>
           </div>
-
-          <button
-            onClick={toggleMenu}
-            className="lg:hidden flex items-center mr-5 px-3 py-2"
-          >
-            <svg
-              className="w-6 h-6 fill-current text-black"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
         </div>
         {location.pathname !== "/checkout" && (
           <span className="text-black mt-1 mr-8">
